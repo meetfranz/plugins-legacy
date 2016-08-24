cb57c4a0bfc75f37cdea8f3c44a0bc2634de0c46
Stefan <stefan@meetfranz.com>
2016-08-24 15:04:46 +0200

    cleanup

--- rocketchat-demo/webview Kopie.js
+++
@@ -1,9 +0,0 @@
-module.exports = (Franz, options) => {
-  function getMessages() {
-    const count = $(".unread-rooms-mode .unread").length;
-
-    Franz.setBadge(count);
-  }
-
-  setInterval(getMessages, 1000);
-}
