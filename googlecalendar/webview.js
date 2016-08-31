4022ae7a35b140b580931f72b7e4ce4f2c91fefb
Florian Keller <github@floriankeller.de>
2016-08-31 19:02:40 +0200

    Catch window.alert and display a modal

--- googlecalendar/webview.js
+++ googlecalendar/webview.js
@@ -1,6 +1,36 @@
 const path = require('path');
 
 module.exports = (Franz, options) => {
+  let updates = 0;
+  const modal = document.createElement('div');
+
+  function showModal (text) {
+    show(modal);
+    modal.querySelector('p').innerHTML = text;
+    updates += 1;
+  }
+
+  function hideModal () {
+    hide(modal);
+    modal.querySelector('p').innerHTML = '';
+    updates -= 1;
+  }
+
+  // Replace window.alert to hide alerts in Franz
+  const oldAlert = window.alert;
+  window.alert = function () {
+    // when Google Calendar displays an alert notify the user
+    showModal.apply(oldAlert, arguments);
+  };
+
+  function show (element) {
+    element.style.display = 'inherit';
+  }
+
+  function hide (element) {
+    element.style.display = 'none';
+  }
+
   const getMessages = () => {
     // get unread messages
     //const updates = document.getElementById('franz').getAttribute('data-unread');
@@ -9,13 +39,20 @@ module.exports = (Franz, options) => {
     //const inbox = document.getElementById('franz').getAttribute('data-inbox');
 
     // set Franz badge
-    // updates => active unread count
-    // inbox => passive unread count
-    Franz.setBadge(0, 0);
+    // updates => passive unread count
+    // inbox => active unread count
+    Franz.setBadge(0, updates);
   };
 
+  modal.id = 'franz-modal';
+  modal.innerHTML = '<div class="modal-content"><span class="close">&times;</span><p></p></div>';
+  modal.querySelector('.close').addEventListener('click', hideModal);
+  document.body.appendChild(modal);
+
+  document.addEventListener('keydown', function(e) { if (e.keyCode === 27) { hideModal(); } })
+
   // inject franz.css stylesheet
-  //Franz.injectCSS(path.join(__dirname, 'css', 'franz.css'));
+  Franz.injectCSS(path.join(__dirname, 'css', 'modal.css'));
 
   // check for new messages every second and update Franz badge
   Franz.loop(getMessages);
