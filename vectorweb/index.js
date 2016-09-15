7bafee8dbb62b5395091521d9f67e5aace514ac3
Tilman Vatteroth <tilman.vatteroth@udo.edu>
2016-09-15 16:02:50 +0200

    only accept vector. not every url

--- vectorweb/index.js
+++ vectorweb/index.js
@@ -1,7 +1,7 @@
 module.exports = (Franz) => {
   class VectorChat extends Franz {
     validateServer(URL) {
-      const api = `${URL}`;
+      const api = `${URL}/config.json`;
       return new Promise((resolve, reject) => {
         $.get(api, (resp) => {
             resolve();
