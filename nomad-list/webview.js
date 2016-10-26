0607c233d09dab1d551cfcf15e937ff071babecf
levels <hello@levels.io>
2016-10-26 19:20:00 +0900

    Add files via upload

--- nomad-list/webview.js
+++ nomad-list/webview.js
@@ -4,6 +4,8 @@ module.exports = (Franz, options) => {
   const getMessages = () => {
     // get unread messages
     // const updates = document.getElementById('franz').getAttribute('data-unread');
+    const count = $(".unread").length;
+    Franz.setBadge(count);
 
     // get conversations in 'My Inbox'
     // const inbox = document.getElementById('franz').getAttribute('data-inbox');
