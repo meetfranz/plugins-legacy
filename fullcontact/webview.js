dadd42ec32c9b0f69b4e55bd822937e996b1bb2a
Patrick Rathke <p@rathke.pro>
2016-11-20 17:33:59 +0100

    fix spaces

--- fullcontact/webview.js
+++ fullcontact/webview.js
@@ -2,13 +2,13 @@ module.exports = (Franz, options) => {
   function getStatus() {
     // Search for activity count
     const elem = document.querySelector('#activity-nav-item span:first-child');
-	const count = elem ? elem.innerHTML : 0;
-	
+    const count = elem ? elem.innerHTML : 0;
+
     // double check if inner HTML was a number
-	if(!isNaN(parseFloat(count)) && isFinite(count)){
+    if(!isNaN(parseFloat(count)) && isFinite(count)){
       Franz.setBadge(count); 
-	}  
+    }  
   }
-  
+
   Franz.loop(getStatus)
 }
