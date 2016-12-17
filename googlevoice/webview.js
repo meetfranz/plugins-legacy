26c2d1e3873dbf51a8aa88a5e9b9fac9422fcfbe
Jason Antman <jason@jasonantman.com>
2016-12-16 21:17:17 -0500

    googlevoice - fix badge to clear when there are no more unread messages

--- googlevoice/webview.js
+++ googlevoice/webview.js
@@ -2,7 +2,9 @@ module.exports = (Franz, options) => {
   function getMessages() {
     const count = document.querySelector('.msgCount').innerHTML.replace(/[\(\) ]/gi,"");
     if (count) {
-		Franz.setBadge(count);
+		  Franz.setBadge(count);
+    } else {
+      Franz.setBadge(0);
     }
   }
 
