f48560f08d9a67b136e9456117777f314695eb3c
Valentyn Riaboshtan <valexar12@gmail.com>
2017-01-20 21:26:18 +0200

    Added toggle  for header.

--- bitrix24/webview.js
+++ bitrix24/webview.js
@@ -1,8 +1,8 @@
-const path = require('path');
-
 if ($('.log-main-table').length == 0 && location.pathname != '/online/') {
   location.pathname = '/online/';
 }
+
+const path = require('path');
 
 module.exports = (Franz, options) => {
 
@@ -15,4 +15,22 @@ module.exports = (Franz, options) => {
 
   Franz.loop(getMessages);
   Franz.injectCSS(path.join(__dirname, "style.css"));
+}
+
+$('#header').after('<button type="button" id="slideUp"></button>');
+
+$(document).on('click', '#slideUp', function(){
+	$('#header').slideToggle();
+	var display = function () {
+		if (localStorage.getItem('display') == 1) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+	localStorage.setItem('display', display());
+});
+
+if (localStorage.getItem('display') == 0) {
+	$('#header').hide();
 }
