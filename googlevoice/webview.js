ebf42ce1fdeb52574276a06de8f2f56817a92b64
Behind The Math <info@behindthemath.io>
2017-01-24 09:51:09 -0500

    Updates the googlevoice plugin to work with the new Google Voice update

--- googlevoice/webview.js
+++ googlevoice/webview.js
@@ -1,8 +1,25 @@
 module.exports = (Franz, options) => {
-  function getMessages() {
-    const count = document.querySelector('.msgCount').innerHTML.replace(/[\(\) ]/gi,"");
-    Franz.setBadge(count ? count : 0);
-  }
+    function parseIntOrZero(val) {
+        return isNaN(parseInt(val)) ? 0 : parseInt(val);
+    }
 
-  Franz.loop(getMessages);
+    function getMessages() {
+        var el = document.querySelector('.msgCount');
+        var count;
+
+        if (el) {
+            // Legacy version of Google Voice
+            count = parseIntOrZero(el.innerHTML.replace(/[\(\) ]/gi,""));
+        } else {
+            // Updated version of Google Voice
+            const count_messages = parseIntOrZero(document.querySelector('gv-nav-button[label="Messages"] div[aria-label="Unread count"]').innerHTML);
+            const count_calls = parseIntOrZero(document.querySelector('gv-nav-button[label="Calls"] div[aria-label="Unread count"]').innerHTML);
+            const count_voicemails = parseIntOrZero(document.querySelector('gv-nav-button[label="Voicemail"] div[aria-label="Unread count"]').innerHTML);
+            count = count_messages + count_calls + count_voicemails;
+        }
+
+        Franz.setBadge(count);
+    }
+
+    Franz.loop(getMessages);
 }
