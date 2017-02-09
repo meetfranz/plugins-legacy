6028c6d996180a297669c1a7b080f3d5e8e1b48f
Alan Plum <me@pluma.io>
2017-02-09 21:42:08 +0100

    Fix external links

--- fastmail/webview.js
+++ fastmail/webview.js
@@ -1,4 +1,14 @@
 module.exports = (Franz, options) => {
+  var interval = setInterval(function () {
+    if (!window.O || !window.O.WindowController) return;
+    window.O.WindowController.openExternal = function (href) {
+      var temp = document.createElement('a');
+      temp.setAttribute('href', href);
+      temp.setAttribute('target', '_blank');
+      temp.click();
+    };
+    clearInterval(interval);
+  }, 200);
   const getMessages = () => {
     const inbox = document.querySelector(".v-FolderSource--inbox>.v-FolderSource-badge");
     if (!inbox) {
