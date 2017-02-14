2ceb02f4bd3625a5176bcd4a4d34a566d6da1770
Valentyn Riaboshtan <valexar12@gmail.com>
2017-02-14 16:56:23 +0200

    Update notify icon for Notification

--- bitrix24/webview.js
+++ bitrix24/webview.js
@@ -1,5 +1,6 @@
 if ($('.log-main-table').length == 0 && location.pathname != '/online/') {
   location.pathname = '/online/';
+  return false;
 }
 
 const path = require('path');
@@ -8,7 +9,7 @@ module.exports = (Franz, options) => {
 
   function getMessages() {
 
-    const directMessages = $(".bx-messenger-cl-item .bx-messenger-cl-count-digit, .bx-messenger-cl-notify-button .bx-messenger-cl-count-digit").length;
+    const directMessages = $(".bx-messenger-cl-item .bx-messenger-cl-count-digit, .bx-messenger-cl-notify-button .bx-messenger-cl-count-digit, #bx-desktop-tab-notify .bx-desktop-tab-counter-digit, #bx-desktop-tab-im-lf .bx-desktop-tab-counter-digit").length;
 
     Franz.setBadge(directMessages);
   }
@@ -35,8 +36,14 @@ if (localStorage.getItem('display') == 0) {
 	$('#header').hide();
 }
 
-$(document).on('click', '#logo_24_a, .bx-messenger-panel-title-link', function(event){
-	window.open(this.href, '__blank');
-	return false;
-});
 $('.bx-layout-table > tbody > tr:first').after($('.bx-im-fullscreen-popup-table .bx-im-fullscreen-popup-td2'));
+
+$(document).ready(function () {
+    $('body').off('click', '#logo_24_a, .bx-messenger-panel-title-link, .bx-notifier-item-text a').on('click', '#logo_24_a, .bx-messenger-panel-title-link, .bx-notifier-item-text a', function(event){
+        window.open(this.href, '__blank');
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        event.preventDefault();
+        return false;
+    });
+});
