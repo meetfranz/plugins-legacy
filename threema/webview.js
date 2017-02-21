d4953bfacef26249d6cb6809c8e27a8e00f086d8
jomority <jo.mority@gmail.com>
2017-02-21 18:57:35 +0100

    added css injection, update unread only when possible

--- threema/webview.js
+++ threema/webview.js
@@ -1,16 +1,33 @@
+const path = require('path');
+
 module.exports = (Franz, options) => {
   function getMessages () {
-    var count = 0
+    var navItems = document.getElementsByClassName("md-nav-item");
 
-    document.querySelectorAll('.unread-count').forEach(element => {
-      const localCount = parseInt(element.innerHTML.replace(/\s+/g, ''))
-      if (localCount > 0) {
-        count++
-      }
-    })
+    var updateMessageCount = false;
 
-    Franz.setBadge(count)
+    for(var i = 0; i < navItems.length; i++) {
+        if(navItems[i].attributes.name.value == "conversations" && navItems[i].attributes["aria-selected"].value == "true") {
+            updateMessageCount = true;
+            break;
+        }
+    }
+
+    if(updateMessageCount) {
+      var count = 0
+
+      document.querySelectorAll('.unread-count').forEach(element => {
+        const localCount = parseInt(element.innerHTML.replace(/\s+/g, ''))
+        if (localCount > 0) {
+          count++
+        }
+      })
+
+      Franz.setBadge(count)
+    }
   }
+
+  Franz.injectCSS(path.join(__dirname, 'franz.css'));
 
   Franz.loop(getMessages)
 }
