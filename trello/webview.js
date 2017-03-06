7654e1e2f1b399b614bbae9c390b62f084d8aad9
Kuyen <iamcxa@gmail.com>
2017-03-06 14:52:41 +0800

    feature - add trello badge notifications based on card duedate notices.

--- trello/webview.js
+++ trello/webview.js
@@ -1,3 +1,7 @@
 module.exports = (Franz, options) => {
-    
+	const getMessages = () => {
+		const unread = document.getElementsByClassName('new-notifications').length;
+		Franz.setBadge(0, unread);
+	}
+	Franz.loop(getMessages);
 }
