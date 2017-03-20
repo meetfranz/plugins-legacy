17ce19dc5b676bf768d9302f8e34dd6a5f8ac80f
Charles Feval <github@crap.feval.fr>
2017-03-20 10:00:54 -0700

    Added direct message count for the twitter plugin

--- twitter/webview.js
+++ twitter/webview.js
@@ -1,8 +1,8 @@
-const path = require('path');
-
 module.exports = (Franz, options) => {
   const getMessages = () => {
-    const newMessage = document.getElementsByClassName("Fe7ul3Lt _2DggF3sL _2izplv41")[0].innerText == "New Tweets";
-    Franz.setBadge(newMessage ? "." : "0");
+    const span = Array.prototype.filter.call(document.getElementsByTagName("a"), (a) => a.href.includes("/messages"))[0].getElementsByTagName("span");
+    const count = span.length > 0 ? span[0].textContent : 0;
+    Franz.setBadge(count);
+  };
   Franz.loop(getMessages);
 };
