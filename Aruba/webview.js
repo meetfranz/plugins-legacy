b9219376f61b2b6a38bd70492c631b111ae9a3d6
DanieleBuccilli <d.buccilli@keplerotech.com>
2017-03-23 10:00:30 +0100

    Minor Fix
    Signed-off-by: DanieleBuccilli <d.buccilli@keplerotech.com>

--- Aruba/webview.js
+++ Aruba/webview.js
@@ -1,12 +1,22 @@
 module.exports = (Franz, options) => {
-	const getMessages = () => {
-		const directMessages = document.getElementById("menuFolderInboxButton").getElementsByClassName("unreadFolders")[0].getElementsByClassName("inner_unread")[0].innerHTML;
-		const totalMessages = document.getElementById("menuFoldersList").getElementsByClassName("unreadFolders")[0].getElementsByClassName("inner_unread")[0].innerHTML;
-		const indirectMessages = totalMessages - directMessages;
+	function getMessages() {
+		var directMessages=0;
+		var totalMessages=0;
+		var folderInbox=document.getElementById("menuFolderInboxButton");
+		var foldersList = document.getElementById("menuFoldersList");
+		
+		if (!folderInbox.classList.contains("noneUnread")){
+			directMessages = folderInbox.getElementsByClassName("unreadFolders")[0].getElementsByClassName("inner_unread")[0].innerHTML;
+		}
+		if (!foldersList.classList.contains("noneUnread")){
+			totalMessages = foldersList.getElementsByClassName("unreadFolders")[0].getElementsByClassName("inner_unread")[0].innerHTML;
+		}
+		
+		var indirectMessages=totalMessages-directMessages
 
 		Franz.setBadge(directMessages, indirectMessages);
 	}
 
-	Franz.loop(getMessages);
+	Franz.loop(getMessages); 
 
 }
