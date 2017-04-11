2a4c7575318143cfc56ac5f621d5b3636145e255
takkanm <takkanm@gmail.com>
2017-04-11 14:34:37 +0900

    support unread badge

--- idobata/webview.js
+++ idobata/webview.js
@@ -1,2 +1,8 @@
 module.exports = (Franz, options) => {
+  function getMessages() {
+    var count = document.querySelector("title").innerHTML.match(/\d+/)[0]
+    Franz.setBadge(count);
+  };
+
+  Franz.loop(getMessages);
 }
