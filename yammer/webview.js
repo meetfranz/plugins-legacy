82bb8f4e150530a0818645d1493af707366a3c34
Sally Young <sally@justafish.co.uk>
2017-05-15 17:42:41 +0100

    Fix badge count once the new message link is clicked

--- yammer/webview.js
+++ yammer/webview.js
@@ -1,22 +1,21 @@
 module.exports = (Franz, options) => {
   const getMessages = () => {
-    // get unread messages
-    const count = $('.yj-thread-list--new-message-text').text();
+    const countText = $('.yj-thread-list--new-message-text:visible').text();
     const regex = /Click to see (\d+) new/g;
-    if (count) {
+    let newMessageCount = 0;
+    if (countText && countText.length) {
       let m;
-      while ((m = regex.exec(count)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
+      while ((m = regex.exec(countText)) !== null) {
         if (m.index === regex.lastIndex) {
           regex.lastIndex++;
         }
-
-        // The result can be accessed through the `m`-variable.
         if (typeof m[1] !== undefined) {
-          Franz.setBadge(m[1]);
+          newMessageCount = m[1];
         }
       }
     }
+
+    Franz.setBadge(newMessageCount);
   };
 
   // check for new messages every second and update Franz badge
