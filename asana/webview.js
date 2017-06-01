8ea0275da48a708c35ac6c23a8949c6680d46920
Ida Bagus Redy Santiawan <goesredy@gmail.com>
2017-06-01 23:32:00 +0800

    Update webview.js

--- asana/webview.js
+++ asana/webview.js
@@ -1,5 +1,9 @@
 module.exports = (Franz, options) => {
   function getMessages() {
+    const googleButton = document.getElementById('google_auth_button');
+    if (googleButton) {
+      googleButton.onclick = function(){ return Dialog.Login.loginWithGoogle(true, 'https://app.asana.com/-/oauth2callback', 'https:\/\/app.asana.com\/') } ;
+    }
     const inbox = document.getElementsByClassName('topbar-notificationsButton has-newNotifications');
     const count = inbox.length;
 
