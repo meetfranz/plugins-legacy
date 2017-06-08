027157ac2a414f74ee58e0ca65338a6be2777856
leandrogehlen <leandrogehlen@gmail.com>
2017-06-08 14:26:10 -0300

    Fixes code style

--- tawkto/webview.js
+++ tawkto/webview.js
@@ -1,9 +1,9 @@
 module.exports = (Franz, options) => {
   function getMessages() {
     var messages = $(".new-message-container"),
-		count = (messages.length) ? messages.nextAll().length -1 : 0;
+        count = (messages.length) ? messages.nextAll().length - 1 : 0;
 
-	Franz.setBadge(count);
+    Franz.setBadge(count);
   }
 
   Franz.loop(getMessages);
