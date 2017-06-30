afb57878c9a7704bd5ba457360bb88b41d956d0e
Kuyen <iamcxa@gmail.com>
2017-06-30 16:56:14 +0800

    update Facebook page

--- facebookpage/webview.js
+++ facebookpage/webview.js
@@ -1,14 +1,23 @@
 const path = require('path');
 
 module.exports = (Franz, options) => {
-	const getValueById = (id) => {
-		return document.getElementById(id).innerText;
-	};
-	const getMessages = () => {
-		const unreadNotices = getValueById('notificationsCountValue');
-		const unreadMessage = getValueById('mercurymessagesCountValue');
-		Franz.setBadge(unreadMessage, unreadNotices);
-	}
-	Franz.loop(getMessages);
-	Franz.injectCSS(path.join(__dirname, 'css', 'style.css'));
+  const getInputsByValue = function (value) {
+    var allInputs = document.getElementsByTagName("a");
+    var results = [];
+    for (var x = 0; x < allInputs.length; x++)
+      if (allInputs[x].getAttribute("href").indexOf(value) !== -1)
+        results.push(allInputs[x]);
+    return results;
+  };
+  const getValueById = (id) => {
+    return document.getElementById(id).innerText;
+  };
+  const getMessages = () => {
+    const unreadNotices = getValueById('notificationsCountValue');
+    const messageParent = getInputsByValue('/trunk.studio.tw/inbox/')
+    const unreadMessage = messageParent[0].children[0].children[1].innerText;
+    Franz.setBadge(unreadMessage, unreadNotices);
+  }
+  Franz.loop(getMessages);
+  Franz.injectCSS(path.join(__dirname, 'css', 'style.css'));
 }
