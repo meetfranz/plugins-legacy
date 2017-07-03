9ef64d77b681bd8b36a176b33c78108a0903985a
Kuyen <iamcxa@gmail.com>
2017-07-03 12:08:41 +0800

    improve facecebook page

--- facebookpage/webview.js
+++ facebookpage/webview.js
@@ -4,9 +4,12 @@ module.exports = (Franz, options) => {
   const getInputsByValue = function (value) {
     var allInputs = document.getElementsByTagName("a");
     var results = [];
-    for (var x = 0; x < allInputs.length; x++)
-      if (allInputs[x].getAttribute("href").indexOf(value) !== -1)
-        results.push(allInputs[x]);
+    for (var x = 0; x < allInputs.length; x++) {
+      if(allInputs[x].getAttribute("href")) {
+        if (allInputs[x].getAttribute("href").indexOf(value) !== -1)
+          results.push(allInputs[x]);
+      }
+    }
     return results;
   };
   const getValueById = (id) => {
@@ -14,8 +17,14 @@ module.exports = (Franz, options) => {
   };
   const getMessages = () => {
     const unreadNotices = getValueById('notificationsCountValue');
-    const messageParent = getInputsByValue('/trunk.studio.tw/inbox/')
-    const unreadMessage = messageParent[0].children[0].children[1].innerText;
+    const messageParent = getInputsByValue('/trunk.studio.tw/inbox/');
+    let unreadMessage = 0;
+    if (messageParent[0] && 
+        messageParent[0].children[0] && 
+        messageParent[0].children[0].children[1]) 
+    {
+      unreadMessage = messageParent[0].children[0].children[1].innerText;
+    }
     Franz.setBadge(unreadMessage, unreadNotices);
   }
   Franz.loop(getMessages);
