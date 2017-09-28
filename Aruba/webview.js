eafc6124d66117c711cc6cc1f03ca649119adb16
DanieleBuccilli <d.buccilli@keplerotech.com>
2017-09-28 09:12:18 +0200

    Fix for the new web mail

--- Aruba/webview.js
+++ Aruba/webview.js
@@ -2,14 +2,14 @@ module.exports = (Franz, options) => {
 	function getMessages() {
 		var directMessages=0;
 		var totalMessages=0;
-		var folderInbox=document.getElementById("menuFolderInboxButton");
-		var foldersList = document.getElementById("menuFoldersList");
+		var folderInboxUnread=document.getElementById("menuFolderInboxButton").getElementsByClassName("unreadFolders")[0].getElementsByClassName("inner_unread")[0];
+		var foldersListUnread = document.getElementById("menuFoldersList").getElementsByClassName("unreadFolders")[0].getElementsByClassName("inner_unread")[0];
 		
-		if (!folderInbox.classList.contains("noneUnread")){
-			directMessages = folderInbox.getElementsByClassName("unreadFolders")[0].getElementsByClassName("inner_unread")[0].innerHTML;
+		if (!folderInboxUnread.classList.contains("removed")){
+			directMessages = folderInboxUnread.innerHTML;
 		}
-		if (!foldersList.classList.contains("noneUnread")){
-			totalMessages = foldersList.getElementsByClassName("unreadFolders")[0].getElementsByClassName("inner_unread")[0].innerHTML;
+		if (!foldersListUnread.classList.contains("removed")){
+			totalMessages = foldersListUnread.innerHTML;
 		}
 		
 		var indirectMessages=totalMessages-directMessages
